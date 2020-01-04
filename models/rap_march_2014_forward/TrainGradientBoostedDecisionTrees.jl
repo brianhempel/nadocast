9ef95a35dffb878deeac72a87c8a11105d7066ad
Brian Hempel <bnhempel@mtu.edu>
2020-01-04 14:21:08 -0600

    Setup for HREF training

--- models/rap_march_2014_forward/TrainGradientBoostedDecisionTrees.jl
+++ models/rap_march_2014_forward/TrainGradientBoostedDecisionTrees.jl
@@ -9,42 +9,26 @@ import RAP
 forecast_hour = parse(Int64, ENV["FORECAST_HOUR"])
 forecast_hour_range = forecast_hour:forecast_hour
 
-model_prefix = "gbdt_f$(forecast_hour)_$(replace(repr(Dates.now()), ":" => "."))"
+model_prefix = "gbdt_3hr_window_3hr_min_mean_max_delta_f$(forecast_hour)_$(replace(repr(Dates.now()), ":" => "."))"
 
-rap_forecasts = RAP.forecasts()
-# rap_forecasts = rap_forecasts[1:200:length(rap_forecasts)] # Subset the data
-
-# 11650 for training. (2107 with tornadoes.)
-# 2278 for validation.
-# 2287 for testing.
-
-# +12 hour model:
-# Validation size: (20829362 * 2926 = 60,946,713,212 byte validation)
-# Annealing round 1 (21382574 * 2926 = 62,565,411,524 bytes training): loss = 0.0012037802 with Dict{Symbol,Real}(:max_depth=>5,:max_delta_score=>2.0,:learning_rate=>0.05,:max_leaves=>12,:l2_regularization=>5.0,:feature_fraction=>0.6,:bagging_temperature=>0.5,:min_data_weight_in_leaf=>15000.0)
-# Stopped. Neither HREF nor SREF benefiting from "annealing".
-# 347:52:48 elapsed (14 days)
-
-
-TrainGBDTShared.train_multiple_annealing_rounds_with_coordinate_descent_hyperparameter_search(
-    rap_forecasts;
+TrainGBDTShared.train_with_coordinate_descent_hyperparameter_search(
+    RAP.three_hour_window_three_hour_min_mean_max_delta_feature_engineered_forecasts();
     forecast_hour_range = forecast_hour_range,
     model_prefix = model_prefix,
-    get_feature_engineered_data = RAP.get_feature_engineered_data,
 
-    annealing_rounds = 3,
-    basal_inclusion_probability = 0.04f0,
-    prediction_inclusion_multiplier = 1000.0f0,
-    validation_inclusion_probability = 0.2f0,
+    training_X_and_labels_to_inclusion_probabilities   = (X, labels) -> max.(0.004f0, labels),
+    validation_X_and_labels_to_inclusion_probabilities = (X, labels) -> max.(0.02f0, labels),
 
     bin_split_forecast_sample_count = 200,
     max_iterations_without_improvement = 20,
 
-    min_data_weight_in_leaf = [10.0, 15.0, 20.0, 35.0, 50.0, 70.0, 100.0, 150.0, 200.0, 350.0, 500.0, 700.0, 1000.0, 1500.0, 2000.0, 3500.0, 5000.0, 7000.0, 10000.0, 15000.0, 20000.0, 35000.0, 50000.0, 70000.0, 100000.0, 150000.0, 200000.0, 350000.0, 500000.0, 700000.0, 1000000.0, 1500000.0, 2000000.0, 3500000.0, 5000000.0, 7000000.0, 10000000.0],
-    l2_regularization       = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 20.0, 40.0, 80.0],
+    # Roughly factors of 1.78 (4 steps per power of 10)
+    min_data_weight_in_leaf = [10.0, 18.0, 32.0, 56.0, 100.0, 180.0, 320.0, 560.0, 1000.0, 1800.0, 3200.0, 5600.0, 10000.0, 18000.0, 32000.0, 56000.0, 100000.0, 180000.0, 320000.0, 560000.0, 1000000.0, 1800000.0, 3200000.0, 5600000.0, 10000000.0],
+    l2_regularization       = [0.0, 0.5, 1.0, 1.8, 3.2, 5.6, 10.0, 18.0, 32.0, 56.0, 100.0],
     max_leaves              = [3, 4, 5, 6, 8, 10, 12, 15, 20, 25, 30, 35],
     max_depth               = [2, 3, 4, 5, 6, 7, 8, 9],
-    max_delta_score         = [0.5, 0.7, 1.0, 1.5, 2.0, 3.0, 5.0, 7.0, 10.0, 15.0, 20.0, 1000.0],
-    learning_rate           = [0.2, 0.15, 0.1, 0.07, 0.05, 0.035, 0.02],
-    feature_fraction        = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0],
+    max_delta_score         = [0.32, 0.56, 1.0, 1.8, 3.2, 5.6, 10.0, 18.0, 32.0, 56.0, 100.0],
+    learning_rate           = [0.016, 0.025, 0.040, 0.063, 0.1, 0.16, 0.25], # factors of 1.585 (5 steps per power of 10)
+    feature_fraction        = [0.1, 0.25, 0.5, 0.75, 1.0],
     bagging_temperature     = [0.0, 0.1, 0.25, 0.5, 0.75, 1.0]
   )
