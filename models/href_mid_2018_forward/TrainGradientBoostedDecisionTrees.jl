3f832a32f98bb7f4590722e03d6404ec65e97861
Brian Hempel <bnhempel@mtu.edu>
2020-08-29 23:30:13 -0400

    Configure HREF training

--- models/href_mid_2018_forward/TrainGradientBoostedDecisionTrees.jl
+++ models/href_mid_2018_forward/TrainGradientBoostedDecisionTrees.jl
@@ -7,58 +7,50 @@ push!(LOAD_PATH, @__DIR__)
 import HREF
 
 
-forecast_hour_range = 1:36 # HREF files come out 2-3 hours after run time
+# HREF files come out 2-3 hours after run time
 
-model_prefix = "gbdt_f$(forecast_hour_range.start)-$(forecast_hour_range.stop)_$(replace(repr(Dates.now()), ":" => "."))"
+forecast_hour_range =
+  if occursin(r"^\d+:\d+$", get(ENV, "FORECAST_HOUR_RANGE", ""))
+    start, stop = map(str -> parse(Int64, str), split(ENV["FORECAST_HOUR_RANGE"], ":"))
+    start:stop
+  else
+    2:35
+  end
 
-href_forecasts = HREF.forecasts()
-# href_forecasts = href_forecasts[1:100:length(href_forecasts)] # Subset the data
+# 2:13
+# 13:24
+# 24:35
 
-# (no multithreading)
-#
-# old compression method:            9   gigs after training data loaded, 14   gigs after validation loaded, 80s per tree (including validation)
-# delta compression method:          9   gigs after training data loaded, 13.5 gigs after validation loaded, 110s per tree (including validation)
-# delta compression + consolidation: 8.3 gigs after training data loaded, 12.8 gigs after validation loaded, 110s per tree (including validation)
-# no compression:                    10  gigs after training data loaded, 16.5 gigs after validation loaded, 50s per tree (including validation)
+data_subset_ratio = parse(Float32, get(ENV, "DATA_SUBSET_RATIO", "0.03"))
 
-# somehow it's better with multithreading...?? Oh, made the change so grids are shared--that may be it. Now only 10.5 w/validation uncompressed. Still 55s per tree...the caching isn't helping.
-# delta compression + consolidation: 4.5 gigs after training loaded, 6 gigs after validation loaded, 60s per tree (4x multithreaded, including validation; caching may be helping here)
+hour_range_str = "f$(forecast_hour_range.start)-$(forecast_hour_range.stop)"
 
-# Validation size: (32119850 * 1847 = 59,325,362,950 byte validation)
-# Annealing round 1 (26673508 * 1847 = 49,265,969,276 bytes training): loss = 0.0012652115 with Dict{Symbol,Real}(:max_depth=>4,:max_delta_score=>5.0,:learning_rate=>0.07,:max_leaves=>6,:l2_regularization=>3.0,:feature_fraction=>0.6,:bagging_temperature=>0.25,:min_data_weight_in_leaf=>10000.0)
-# Annealing round 2 (35392152 * 1847 = 65,369,304,744 bytes training): loss = 0.0012822518 with Dict{Symbol,Real}(:max_depth=>5,:max_delta_score=>3.0,:learning_rate=>0.07,:max_leaves=>10,:l2_regularization=>3.0,:feature_fraction=>0.6,:bagging_temperature=>0.25,:min_data_weight_in_leaf=>10000.0)
-# Annealing round 3 (35605258 * 1847 = 65,762,911,526 bytes training): loss = 0.0012793123 with Dict{Symbol,Real}(:max_depth=>4,:max_delta_score=>3.0,:learning_rate=>0.05,:max_leaves=>6,:l2_regularization=>2.0,:feature_fraction=>0.7,:bagging_temperature=>0.25,:min_data_weight_in_leaf=>15000.0)
-# Annealing: 143:13:46 elapsed
+model_prefix = "gbdt_3hr_window_3hr_min_mean_max_delta_$(hour_range_str)_$(replace(repr(Dates.now()), ":" => "."))"
 
-TrainGBDTShared.train_multiple_annealing_rounds_with_coordinate_descent_hyperparameter_search(
-    href_forecasts;
+# $ FORECAST_HOUR_RANGE=2:13 DATA_SUBSET_RATIO=0.03 make train_gradient_boosted_decision_trees
+
+TrainGBDTShared.train_with_coordinate_descent_hyperparameter_search(
+    HREF.three_hour_window_three_hour_min_mean_max_delta_feature_engineered_forecasts();
     forecast_hour_range = forecast_hour_range,
     model_prefix = model_prefix,
-    get_feature_engineered_data = HREF.get_feature_engineered_data,
+    save_dir     = "href_$(hour_range_str)_$(data_subset_ratio)",
 
-    annealing_rounds = 3,
-    basal_inclusion_probability = 0.1f0,
-    prediction_inclusion_multiplier = 1000f0,
-    validation_inclusion_probability = 0.5f0,
+    training_X_and_labels_to_inclusion_probabilities   = (X, labels) -> max.(data_subset_ratio, labels),
+    validation_X_and_labels_to_inclusion_probabilities = (X, labels) -> max.(data_subset_ratio, labels),
 
-    bin_split_forecast_sample_count = 200,
+    bin_split_forecast_sample_count    = 200,
     max_iterations_without_improvement = 20,
 
-    min_data_weight_in_leaf = [10.0, 15.0, 20.0, 35.0, 50.0, 70.0, 100.0, 150.0, 200.0, 350.0, 500.0, 700.0, 1000.0, 1500.0, 2000.0, 3500.0, 5000.0, 7000.0, 10000.0, 15000.0, 20000.0, 35000.0, 50000.0, 70000.0, 100000.0, 150000.0, 200000.0, 350000.0, 500000.0, 700000.0, 1000000.0, 1500000.0, 2000000.0, 3500000.0, 5000000.0, 7000000.0, 10000000.0],
-    l2_regularization       = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 20.0, 40.0, 80.0],
-    max_leaves              = [3, 4, 5, 6, 8, 10, 12, 15, 20, 25, 30, 35],
-    max_depth               = [2, 3, 4, 5, 6, 7, 8, 9],
-    max_delta_score         = [0.5, 0.7, 1.0, 1.5, 2.0, 3.0, 5.0, 7.0, 10.0, 15.0, 20.0, 1000.0],
-    learning_rate           = [0.2, 0.15, 0.1, 0.07, 0.05, 0.035, 0.02],
-    feature_fraction        = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0],
-    bagging_temperature     = [0.0, 0.1, 0.25, 0.5, 0.75, 1.0]
+    # Start with middle value for each parameter, plus some number of random choices, before beginning coordinate descent.
+    random_start_count = 20,
 
-    # min_data_weight_in_leaf = [20000.0, 35000.0, 50000.0, 70000.0, 100000.0, 150000.0, 200000.0, 350000.0, 500000.0, 700000.0, 1000000.0, 1500000.0, 2000000.0, 3500000.0, 5000000.0],
-    # l2_regularization       = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 20.0, 40.0, 80.0],
-    # max_leaves              = [3, 4, 5, 6, 8, 10, 12, 15],
-    # max_depth               = [2, 3, 4, 5, 6, 7],
-    # max_delta_score         = [1.0, 1.5, 2.0, 3.0, 5.0, 7.0, 10.0, 15.0, 20.0, 1000.0],
-    # learning_rate           = [0.1, 0.07, 0.05, 0.03, 0.02, 0.015, 0.01],
-    # feature_fraction        = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0],
-    # bagging_temperature     = [0.0, 0.1, 0.25, 0.5, 0.75, 1.0]
+    # Roughly factors of 1.78 (4 steps per power of 10)
+    min_data_weight_in_leaf     = [100.0, 180.0, 320.0, 560.0, 1000.0, 1800.0, 3200.0, 5600.0, 10000.0, 18000.0, 32000.0, 56000.0, 100000.0, 180000.0, 320000.0, 560000.0, 1000000.0, 1800000.0, 3200000.0, 5600000.0, 10000000.0],
+    l2_regularization           = [3.2],
+    max_leaves                  = [2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 25, 30, 35],
+    max_depth                   = [3, 4, 5, 6, 7, 8],
+    max_delta_score             = [0.56, 1.0, 1.8, 3.2, 5.6],
+    learning_rate               = [0.063], # [0.025, 0.040, 0.063, 0.1, 0.16], # factors of 1.585 (5 steps per power of 10)
+    feature_fraction            = [0.1, 0.25, 0.5, 0.75, 1.0],
+    bagging_temperature         = [0.25]
   )
--- models/href_mid_2018_forward/TrainGradientBoostedDecisionTrees2019.jl
+++ models/href_mid_2018_forward/TrainGradientBoostedDecisionTrees2019.jl
@@ -7,29 +7,58 @@ push!(LOAD_PATH, @__DIR__)
 import HREF
 
 
-forecast_hour_range = 2:35 # HREF files come out 2-3 hours after run time
+forecast_hour_range = 1:36 # HREF files come out 2-3 hours after run time
 
-model_prefix = "gbdt_3hr_window_3hr_min_mean_max_delta_f$(forecast_hour_range.start)-$(forecast_hour_range.stop)_$(replace(repr(Dates.now()), ":" => "."))"
+model_prefix = "gbdt_f$(forecast_hour_range.start)-$(forecast_hour_range.stop)_$(replace(repr(Dates.now()), ":" => "."))"
 
+href_forecasts = HREF.forecasts()
+# href_forecasts = href_forecasts[1:100:length(href_forecasts)] # Subset the data
 
-TrainGBDTShared.train_with_coordinate_descent_hyperparameter_search(
-    HREF.three_hour_window_three_hour_min_mean_max_delta_feature_engineered_forecasts();
+# (no multithreading)
+#
+# old compression method:            9   gigs after training data loaded, 14   gigs after validation loaded, 80s per tree (including validation)
+# delta compression method:          9   gigs after training data loaded, 13.5 gigs after validation loaded, 110s per tree (including validation)
+# delta compression + consolidation: 8.3 gigs after training data loaded, 12.8 gigs after validation loaded, 110s per tree (including validation)
+# no compression:                    10  gigs after training data loaded, 16.5 gigs after validation loaded, 50s per tree (including validation)
+
+# somehow it's better with multithreading...?? Oh, made the change so grids are shared--that may be it. Now only 10.5 w/validation uncompressed. Still 55s per tree...the caching isn't helping.
+# delta compression + consolidation: 4.5 gigs after training loaded, 6 gigs after validation loaded, 60s per tree (4x multithreaded, including validation; caching may be helping here)
+
+# Validation size: (32119850 * 1847 = 59,325,362,950 byte validation)
+# Annealing round 1 (26673508 * 1847 = 49,265,969,276 bytes training): loss = 0.0012652115 with Dict{Symbol,Real}(:max_depth=>4,:max_delta_score=>5.0,:learning_rate=>0.07,:max_leaves=>6,:l2_regularization=>3.0,:feature_fraction=>0.6,:bagging_temperature=>0.25,:min_data_weight_in_leaf=>10000.0)
+# Annealing round 2 (35392152 * 1847 = 65,369,304,744 bytes training): loss = 0.0012822518 with Dict{Symbol,Real}(:max_depth=>5,:max_delta_score=>3.0,:learning_rate=>0.07,:max_leaves=>10,:l2_regularization=>3.0,:feature_fraction=>0.6,:bagging_temperature=>0.25,:min_data_weight_in_leaf=>10000.0)
+# Annealing round 3 (35605258 * 1847 = 65,762,911,526 bytes training): loss = 0.0012793123 with Dict{Symbol,Real}(:max_depth=>4,:max_delta_score=>3.0,:learning_rate=>0.05,:max_leaves=>6,:l2_regularization=>2.0,:feature_fraction=>0.7,:bagging_temperature=>0.25,:min_data_weight_in_leaf=>15000.0)
+# Annealing: 143:13:46 elapsed
+
+TrainGBDTShared.train_multiple_annealing_rounds_with_coordinate_descent_hyperparameter_search(
+    href_forecasts;
     forecast_hour_range = forecast_hour_range,
     model_prefix = model_prefix,
+    get_feature_engineered_data = HREF.get_feature_engineered_data,
 
-    training_X_and_labels_to_inclusion_probabilities   = (X, labels) -> max.(0.01f0, labels),
-    validation_X_and_labels_to_inclusion_probabilities = (X, labels) -> max.(0.05f0, labels),
+    annealing_rounds = 3,
+    basal_inclusion_probability = 0.1f0,
+    prediction_inclusion_multiplier = 1000f0,
+    validation_inclusion_probability = 0.5f0,
 
-    bin_split_forecast_sample_count    = 200,
+    bin_split_forecast_sample_count = 200,
     max_iterations_without_improvement = 20,
 
-    # Roughly factors of 1.78 (4 steps per power of 10)
-    min_data_weight_in_leaf = [10.0, 18.0, 32.0, 56.0, 100.0, 180.0, 320.0, 560.0, 1000.0, 1800.0, 3200.0, 5600.0, 10000.0, 18000.0, 32000.0, 56000.0, 100000.0, 180000.0, 320000.0, 560000.0, 1000000.0, 1800000.0, 3200000.0, 5600000.0, 10000000.0],
-    l2_regularization       = [0.0, 0.5, 1.0, 1.8, 3.2, 5.6, 10.0, 18.0, 32.0, 56.0, 100.0],
+    min_data_weight_in_leaf = [10.0, 15.0, 20.0, 35.0, 50.0, 70.0, 100.0, 150.0, 200.0, 350.0, 500.0, 700.0, 1000.0, 1500.0, 2000.0, 3500.0, 5000.0, 7000.0, 10000.0, 15000.0, 20000.0, 35000.0, 50000.0, 70000.0, 100000.0, 150000.0, 200000.0, 350000.0, 500000.0, 700000.0, 1000000.0, 1500000.0, 2000000.0, 3500000.0, 5000000.0, 7000000.0, 10000000.0],
+    l2_regularization       = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 20.0, 40.0, 80.0],
     max_leaves              = [3, 4, 5, 6, 8, 10, 12, 15, 20, 25, 30, 35],
     max_depth               = [2, 3, 4, 5, 6, 7, 8, 9],
-    max_delta_score         = [0.32, 0.56, 1.0, 1.8, 3.2, 5.6, 10.0, 18.0, 32.0, 56.0, 100.0],
-    learning_rate           = [0.016, 0.025, 0.040, 0.063, 0.1, 0.16, 0.25], # factors of 1.585 (5 steps per power of 10)
-    feature_fraction        = [0.1, 0.25, 0.5, 0.75, 1.0],
+    max_delta_score         = [0.5, 0.7, 1.0, 1.5, 2.0, 3.0, 5.0, 7.0, 10.0, 15.0, 20.0, 1000.0],
+    learning_rate           = [0.2, 0.15, 0.1, 0.07, 0.05, 0.035, 0.02],
+    feature_fraction        = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0],
     bagging_temperature     = [0.0, 0.1, 0.25, 0.5, 0.75, 1.0]
+
+    # min_data_weight_in_leaf = [20000.0, 35000.0, 50000.0, 70000.0, 100000.0, 150000.0, 200000.0, 350000.0, 500000.0, 700000.0, 1000000.0, 1500000.0, 2000000.0, 3500000.0, 5000000.0],
+    # l2_regularization       = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 20.0, 40.0, 80.0],
+    # max_leaves              = [3, 4, 5, 6, 8, 10, 12, 15],
+    # max_depth               = [2, 3, 4, 5, 6, 7],
+    # max_delta_score         = [1.0, 1.5, 2.0, 3.0, 5.0, 7.0, 10.0, 15.0, 20.0, 1000.0],
+    # learning_rate           = [0.1, 0.07, 0.05, 0.03, 0.02, 0.015, 0.01],
+    # feature_fraction        = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0],
+    # bagging_temperature     = [0.0, 0.1, 0.25, 0.5, 0.75, 1.0]
   )
