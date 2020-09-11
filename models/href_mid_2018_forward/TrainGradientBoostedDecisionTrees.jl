674883ee58b81c01da35bda648d9f4e4a5d4f37f
Brian Hempel <bnhempel@mtu.edu>
2020-09-10 22:18:36 -0400

    Training logs

--- models/href_mid_2018_forward/TrainGradientBoostedDecisionTrees.jl
+++ models/href_mid_2018_forward/TrainGradientBoostedDecisionTrees.jl
@@ -56,7 +56,29 @@ model_prefix = "gbdt_3hr_window_3hr_min_mean_max_delta_$(hour_range_str)_$(repla
 # 102:45:03 elapsed
 
 # $ FORECAST_HOUR_RANGE=13:24 DATA_SUBSET_RATIO=0.025 make train_gradient_boosted_decision_trees
-
+# ulimit -n 8192; JULIA_NUM_THREADS=16 time julia --project=../.. TrainGradientBoostedDecisionTrees.jl
+# Loading tornadoes...
+# Loading wind events...
+# Loading hail events...
+# 11141 for training. (1967 with tornadoes.)
+# 2268 for validation.
+# 2137 for testing.
+# Preparing bin splits by sampling 200 training tornado hour forecasts
+# filtering to balance 6550 positive and 71439 negative labels...computing bin splits...done.
+# Loading training data
+# done. 10105131 datapoints with 17758 features each.
+# Loading validation data
+# done. 2061642 datapoints with 17758 features each.
+# 
+# Middle config:
+# New best! Loss: 0.0011394698
+# Dict{Symbol,Real}(:max_depth => 5,:max_delta_score => 1.8,:learning_rate => 0.063,:max_leaves => 10,:l2_regularization => 3.2,:feature_fraction => 0.5,:bagging_temperature => 0.25,:min_data_weight_in_leaf => 32000.0)
+# 
+# Remained the best config through random search and coordinate descent.
+# 
+# Best hyperparameters (loss = 0.0011394698):
+# Dict{Symbol,Real}(:max_depth => 5,:max_delta_score => 1.8,:learning_rate => 0.063,:max_leaves => 10,:l2_regularization => 3.2,:feature_fraction => 0.5,:bagging_temperature => 0.25,:min_data_weight_in_leaf => 32000.0)
+# 121:39:50 elapsed
 
 
 TrainGBDTShared.train_with_coordinate_descent_hyperparameter_search(
