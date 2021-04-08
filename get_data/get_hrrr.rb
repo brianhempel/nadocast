2ee234cc34c6476c5260ee62dda99dcfe51ac49c
Brian Hempel <bnhempel@mtu.edu>
2021-04-07 20:36:15 -0500

    get some validation data

--- get_data/get_hrrr.rb
+++ get_data/get_hrrr.rb
@@ -24,21 +24,24 @@ DELETE_UNNEEDED = ARGV.include?("--delete-unneeded") # Delete files in time rang
 # FORECAST_HOURS=1,2,3,6,12,18 ruby get_hrrr.rb --from-archive --delete-unneeded
 # THREAD_COUNT=2 FORECAST_HOURS=2,5,6,7,12,18 ruby get_hrrr.rb --from-archive --delete-unneeded
 # FORECAST_HOURS=2,6,12,16,17,18 ruby get_hrrr.rb --from-archive --delete-unneeded
+# THREAD_COUNT=3 FORECAST_HOURS=11,12,13,16,17,18 ruby get_hrrr.rb --from-archive --delete-unneeded
 
 # Forecaster runs these:
 # RUN_HOURS=8,9,10   FORECAST_HOURS=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 ruby get_hrrr.rb
 # RUN_HOURS=12,13,14 FORECAST_HOURS=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 ruby get_hrrr.rb
 
 # For getting the HRRRs associated with the SREF/HREF forecasts use this (although we don't have these, currently)
-# START_DATE=2018-6-25 FORECAST_HOURS=1,2,3,5,6,7,11,12,13,16,17,18 ruby get_hrrr.rb --from-archive
+# START_DATE=2018-8-24 FORECAST_HOURS=1,2,3,5,6,7,11,12,13,16,17,18 ruby get_hrrr.rb --from-archive
 
 # Want three hour windows. Don't have storage yet for all, so we'll start with +11,+12,+13 and build from there.
 
-RUN_HOURS        = ENV["RUN_HOURS"]&.split(",")&.map(&:to_i) || (0..23).to_a
-FORECAST_HOURS   = ENV["FORECAST_HOURS"]&.split(",")&.map(&:to_i) || [2, 6, 11, 12, 13, 18]
-MIN_FILE_BYTES   = 80_000_000
-THREAD_COUNT     = Integer((DRY_RUN && "1") || ENV["THREAD_COUNT"] || (FROM_ARCHIVE ? "1" : "4"))
-HALF_WINDOW_SIZE = 90*MINUTE # Grab forecasts valid within this many minutes of a geocoded storm event
+RUN_HOURS            = ENV["RUN_HOURS"]&.split(",")&.map(&:to_i) || (0..23).to_a
+FORECAST_HOURS       = ENV["FORECAST_HOURS"]&.split(",")&.map(&:to_i) || [2, 6, 11, 12, 13, 18]
+VALIDATION_RUN_HOURS = ENV["VALIDATION_RUN_HOURS"]&.split(",")&.map(&:to_i) || []
+MIN_FILE_BYTES       = 80_000_000
+THREAD_COUNT         = Integer((DRY_RUN && "1") || ENV["THREAD_COUNT"] || (FROM_ARCHIVE ? "1" : "4"))
+HALF_WINDOW_SIZE     = 90*MINUTE # Grab forecasts valid within this many minutes of a geocoded storm event
+
 
 loop { break if Dir.exists?("/Volumes/HRRR_1/"); puts "Waiting for HRRR_1 to mount..."; sleep 4 }
 loop { break if Dir.exists?("/Volumes/HRRR_2/"); puts "Waiting for HRRR_2 to mount..."; sleep 4 }
@@ -153,7 +156,8 @@ if FROM_ARCHIVE # Storm event hours only, for now. Would be 12TB for all +2 +6 +
   start_date_parts = ENV["START_DATE"]&.split("-")&.map(&:to_i) || [2016,8,24]
 
   # https://pando-rgw01.chpc.utah.edu/hrrr/sfc/20180101/hrrr.t00z.wrfsfcf00.grib2
-  DATES = (Date.new(*start_date_parts)..Date.today).to_a
+  DATES     = (Date.new(*start_date_parts)..Date.today).to_a
+  SATURDAYS = DATES.select(&:saturday?)
 
   storm_event_times =
     conus_event_hours_set(STORM_EVENTS, HALF_WINDOW_SIZE)
@@ -170,6 +174,13 @@ if FROM_ARCHIVE # Storm event hours only, for now. Would be 12TB for all +2 +6 +
          storm_event_times.include?(forecast.valid_time + HOUR) ||
          storm_event_times.include?(forecast.valid_time - HOUR))
     end
+
+  validation_forecasts_to_get =
+    SATURDAYS.product(VALIDATION_RUN_HOURS, (1..30).to_a).map do |date, run_hour, forecast_hour|
+      HRRRForecast.new(date, run_hour, forecast_hour)
+    end
+
+  forecasts_to_get = (forecasts_to_get + validation_forecasts_to_get).uniq
 
   forecasts_to_remove = DELETE_UNNEEDED ? (forecasts_in_range - forecasts_to_get) : []
 
