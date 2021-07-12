f268f5907e27de9e0e403878d4542e302df1b7d0
Brian Hempel <bnhempel@mtu.edu>
2021-07-11 22:54:50 -0500

    Calibrating combined HREF/SREF hourly

--- models/combined_href_sref/CombinedHREFSREF.jl
+++ models/combined_href_sref/CombinedHREFSREF.jl
@@ -34,6 +34,7 @@ HOUR   = 60*MINUTE
 
 _forecasts_href_newer = []
 _forecasts_sref_newer = []
+_forecasts_href_newer_combined = []
 
 # SREF 3 hours behind HREF
 function forecasts_href_newer()
@@ -55,6 +56,17 @@ function forecasts_sref_newer()
   end
 end
 
+# SREF 3 hours behind HREF
+function forecasts_href_newer_combined()
+  if isempty(_forecasts_href_newer_combined)
+    reload_forecasts()
+    _forecasts_href_newer_combined
+  else
+    _forecasts_href_newer_combined
+  end
+end
+
+
 function example_forecast()
   forecasts()[1]
 end
@@ -63,11 +75,33 @@ function grid()
   HREF.grid()
 end
 
+# Bin 1-2 --------
+# -1.0 < HREF_ŷ <= 0.0040673064
+# Fit logistic coefficients: Float32[0.77540565, 0.19299681, -0.21271989]
+bin_1_2_predict(href_ŷ, sref_ŷ) = 0.77540565*logit(href_ŷ) + 0.19299681*logit(sref_ŷ)* + -0.21271989f0
+# Bin 2-3 --------
+# 0.000962529 < HREF_ŷ <= 0.009957244
+# Fit logistic coefficients: Float32[0.84564245, 0.14841641, -0.06817224]
+bin_2_3_predict(href_ŷ, sref_ŷ) = 0.84564245*logit(href_ŷ) + 0.14841641*logit(sref_ŷ)* + -0.06817224
+# Bin 3-4 --------
+# 0.0040673064 < HREF_ŷ <= 0.020302918
+# Fit logistic coefficients: Float32[0.9977281, 0.14388186, 0.64254296]
+bin_3_4_predict(href_ŷ, sref_ŷ) = 0.9977281*logit(href_ŷ) + 0.14388186*logit(sref_ŷ)* + 0.64254296
+# Bin 4-5 --------
+# 0.009957244 < HREF_ŷ <= 0.037081156
+# Fit logistic coefficients: Float32[1.3795987, 0.091625534, 1.9759048]
+bin_4_5_predict(href_ŷ, sref_ŷ) = 1.3795987*logit(href_ŷ) + 0.091625534*logit(sref_ŷ)* + 1.9759048
+# Bin 5-6 --------
+# 0.020302918 < HREF_ŷ <= 1.0
+# Fit logistic coefficients: Float32[0.9358031, 0.1812378, 0.836498]
+bin_5_6_predict(href_ŷ, sref_ŷ) = 0.9358031*logit(href_ŷ) + 0.1812378*logit(sref_ŷ)* + 0.836498
+
 function reload_forecasts()
   # href_paths = Grib2.all_grib2_file_paths_in("/Volumes/SREF_HREF_1/href")
 
   global _forecasts_href_newer
   global _forecasts_sref_newer
+  global _forecasts_href_newer_combined
 
   _forecasts_href_newer = []
   _forecasts_sref_newer = []
@@ -129,6 +163,54 @@ function reload_forecasts()
 
   _forecasts_href_newer = ForecastCombinators.concat_forecasts(paired_href_newer)
   _forecasts_sref_newer = ForecastCombinators.concat_forecasts(paired_sref_newer)
+
+  σ(x) = 1.0f0 / (1.0f0 + exp(-x))
+
+  logit(p) = log(p / (one(p) - p))
+
+  ratio_between(x, lo, hi) = (x - lo) / (hi - lo)
+
+  predict(forecasts, data) = begin
+    href_ŷs = @view data[:,1]
+    sref_ŷs = @view data[:,2]
+
+    out = Array{Float32}(undef, length(href_prediction))
+
+    bin_maxes = Float32[0.000962529, 0.0040673064, 0.009957244, 0.020302918, 0.037081156, 1.0]
+
+    Threads.@threads for i in 1:length(href_prediction)
+      href_ŷ = href_ŷs[i]
+      sref_ŷ = sref_ŷs[i]
+      if href_ŷ <= bin_maxes[1]
+        # Bin 1-2 predictor only
+        ŷ = bin_1_2_predict(href_ŷ, sref_ŷ)
+      elseif href_ŷ <= bin_maxes[2]
+        # Bin 1-2 and 2-3 predictors
+        ratio = ratio_between(href_ŷ, bin_maxes[1], bin_maxes[2])
+        ŷ = ratio*bin_2_3_predict(href_ŷ, sref_ŷ) + (1f0 - ratio)*bin_1_2_predict(href_ŷ, sref_ŷ)
+      elseif href_ŷ <= bin_maxes[3]
+        # Bin 2-3 and 3-4 predictors
+        ratio = ratio_between(href_ŷ, bin_maxes[2], bin_maxes[3])
+        ŷ = ratio*bin_3_4_predict(href_ŷ, sref_ŷ) + (1f0 - ratio)*bin_2_3_predict(href_ŷ, sref_ŷ)
+      elseif href_ŷ <= bin_maxes[4]
+        # Bin 3-4 and 4-5 predictors
+        ratio = ratio_between(href_ŷ, bin_maxes[3], bin_maxes[4])
+        ŷ = ratio*bin_4_5_predict(href_ŷ, sref_ŷ) + (1f0 - ratio)*bin_3_4_predict(href_ŷ, sref_ŷ)
+      elseif href_ŷ <= bin_maxes[5]
+        # Bin 4-5 and 5-6 predictors
+        ratio = ratio_between(href_ŷ, bin_maxes[4], bin_maxes[5])
+        ŷ = ratio*bin_5_6_predict(href_ŷ, sref_ŷ) + (1f0 - ratio)*bin_4_5_predict(href_ŷ, sref_ŷ)
+      else
+        # Bin 5-6 predictor only
+        ŷ = bin_5_6_predict(href_ŷ, sref_ŷ)
+      end
+      out[i] = ŷ
+    end
+
+    out
+  end
+
+  _forecasts_href_newer_combined = PredictionForecasts.simple_prediction_forecasts(_forecasts_href_newer, predict)
 
   ()
 end
