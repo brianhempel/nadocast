b468272a6e531c75ffa1004b70076f55795c30fa
Brian Hempel <bnhempel@mtu.edu>
2021-07-14 23:58:39 -0500

    setup for calibrating RAP prediction forecasts

---
+++ models/rap_prediction/RAPPrediction.jl
@@ -0,0 +1,115 @@
+module RAPPrediction
+
+import Dates
+
+import MemoryConstrainedTreeBoosting
+
+push!(LOAD_PATH, (@__DIR__) * "/../../lib")
+
+import Forecasts
+import ForecastCombinators
+import Grids
+import Inventories
+
+push!(LOAD_PATH, (@__DIR__) * "/../shared")
+import PredictionForecasts
+import Climatology
+import FeatureEngineeringShared
+
+
+push!(LOAD_PATH, (@__DIR__) * "/../rap_march_2014_forward")
+import RAP
+
+
+_forecasts = [] # Raw, unblurred predictions
+_forecasts_with_blurs_and_forecast_hour = [] # For Train.jl
+_forecasts_blurred = [] # For downstream combination with other forecasts
+
+blur_radii = [10, 15, 25, 35, 50, 70]
+
+# # Determined in Train.jl
+# blur_radius_f2  =
+# blur_radius_f17 =
+
+function forecasts()
+  if isempty(_forecasts)
+    reload_forecasts()
+    _forecasts
+  else
+    _forecasts
+  end
+end
+
+function example_forecast()
+  forecasts()[1]
+end
+
+function grid()
+  RAP.grid()
+end
+
+function forecasts_with_blurs_and_forecast_hour()
+  if isempty(_forecasts_with_blurs_and_forecast_hour)
+    reload_forecasts()
+    _forecasts_with_blurs_and_forecast_hour
+  else
+    _forecasts_with_blurs_and_forecast_hour
+  end
+end
+
+function forecasts_blurred()
+  if isempty(_forecasts_blurred)
+    reload_forecasts()
+    _forecasts_blurred
+  else
+    _forecasts_blurred
+  end
+end
+
+function reload_forecasts()
+  global _forecasts
+  global _forecasts_with_blurs_and_forecast_hour
+  global _forecasts_blurred
+
+  _forecasts = []
+
+  rap_forecasts = RAP.three_hour_window_three_hour_min_mean_max_delta_feature_engineered_forecasts()
+
+  predict_f2  = MemoryConstrainedTreeBoosting.load_unbinned_predictor((@__DIR__) * "/../rap_march_2014_forward/gbdt_3hr_window_3hr_min_mean_max_delta_f2_2021-06-11T14.38.30.5/445_trees_loss_0.00081564416.model")
+  predict_f6  = MemoryConstrainedTreeBoosting.load_unbinned_predictor((@__DIR__) * "/../rap_march_2014_forward/gbdt_3hr_window_3hr_min_mean_max_delta_f6_2021-06-04T14.25.56.451/297_trees_loss_0.0009202203.model")
+  predict_f12 = MemoryConstrainedTreeBoosting.load_unbinned_predictor((@__DIR__) * "/../rap_march_2014_forward/gbdt_3hr_window_3hr_min_mean_max_delta_f12_2021-06-07T05.23.57.904/477_trees_loss_0.0009632701.model")
+  predict_f17 = MemoryConstrainedTreeBoosting.load_unbinned_predictor((@__DIR__) * "/../rap_march_2014_forward/gbdt_3hr_window_3hr_min_mean_max_delta_f17_2021-06-03T15.44.45.541/243_trees_loss_0.0009980382.model")
+
+  predict(forecast, data) = begin
+    if forecast.forecast_hour <= 2
+      predict_f2(data)
+    elseif forecast.forecast_hour in 3:5
+      PredictionForecasts.weighted_prediction_between_models_at_different_forecast_hours(forecast, data, 2, 6, predict_f2, predict_f6)
+    elseif forecast.forecast_hour == 6
+      predict_f6(data)
+    elseif forecast.forecast_hour in 7:11
+      PredictionForecasts.weighted_prediction_between_models_at_different_forecast_hours(forecast, data, 6, 12, predict_f6, predict_f12)
+    elseif forecast.forecast_hour == 12
+      predict_f12(data)
+    elseif forecast.forecast_hour in 13:16
+      PredictionForecasts.weighted_prediction_between_models_at_different_forecast_hours(forecast, data, 12, 17, predict_f12, predict_f17)
+    elseif forecast.forecast_hour >= 17
+      predict_f17(data)
+    end
+  end
+
+  _forecasts = PredictionForecasts.simple_prediction_forecasts(rap_forecasts, predict; inventory_misc = "calculated prob")
+
+  _forecasts_with_blurs_and_forecast_hour = PredictionForecasts.with_blurs_and_forecast_hour(_forecasts, blur_radii)
+
+  # grid = _forecasts[1].grid
+
+  # blur_lo_grid_is = Grids.radius_grid_is(grid, Float64(blur_radius_f2))
+  # blur_hi_grid_is = Grids.radius_grid_is(grid, Float64(blur_radius_f17))
+
+  # _forecasts_blurred = PredictionForecasts.blurred(_forecasts, 2:17, blur_lo_grid_is, blur_hi_grid_is)
+
+  ()
+end
+
+end # module RAPPrediction
---
+++ models/rap_prediction/Train.jl
@@ -0,0 +1,222 @@
+# Run this file in a Julia REPL bit by bit.
+#
+# Poor man's notebook.
+
+# $ sshfs -o debug,sshfs_debug,loglevel=debug brian@nadocaster2:/Volumes/ ~/nadocaster2/
+# $ FORECASTS_ROOT=/home/brian/nadocaster2/ make julia
+
+import Dates
+
+push!(LOAD_PATH, (@__DIR__) * "/../shared")
+# import TrainGBDTShared
+import TrainingShared
+using Metrics
+
+push!(LOAD_PATH, @__DIR__)
+import RAPPrediction
+
+push!(LOAD_PATH, (@__DIR__) * "/../../lib")
+import Forecasts
+
+
+(_, validation_forecasts, _) = TrainingShared.forecasts_train_validation_test(RAPPrediction.forecasts_with_blurs_and_forecast_hour(); just_hours_near_storm_events = false);
+
+# We don't have storm events past this time.
+cutoff = Dates.DateTime(2020, 11, 1, 0)
+validation_forecasts = filter(forecast -> Forecasts.valid_utc_datetime(forecast) < cutoff, validation_forecasts); #
+validation_forecasts = filter(forecast -> forecast.forecast_hour in 2:17, validation_forecasts); #
+
+# const ε = 1e-15 # Smallest Float64 power of 10 you can add to 1.0 and not round off to 1.0
+const ε = 1f-7 # Smallest Float32 power of 10 you can add to 1.0 and not round off to 1.0
+logloss(y, ŷ) = -y*log(ŷ + ε) - (1.0f0 - y)*log(1.0f0 - ŷ + ε)
+
+X, y, weights = TrainingShared.get_data_labels_weights(validation_forecasts; save_dir = "validation_forecasts_with_blurs_and_forecast_hour");
+
+length(validation_forecasts) #
+size(X) #
+length(y) #
+
+println("Dividing into bins of equal positive weight...")
+
+ŷ = X[:, 1]
+
+sort_perm = sortperm(ŷ);
+X       = X[sort_perm, :]
+y       = y[sort_perm]
+ŷ       = ŷ[sort_perm]
+weights = weights[sort_perm]
+
+# total_logloss = sum(logloss.(y, ŷ) .* weights)
+total_positive_weight = sum(y .* weights)
+
+bin_count = 6 # equal logloss bins
+per_bin_pos_weight = total_positive_weight / bin_count
+
+bins_Σŷ      = map(_ -> 0.0f0, 1:bin_count)
+bins_Σy      = map(_ -> 0.0f0, 1:bin_count)
+bins_Σweight = map(_ -> 0.0f0, 1:bin_count)
+bins_max     = map(_ -> 1.0f0, 1:bin_count)
+
+bin_i = 1
+# bin_logloss = 0.0
+for i in 1:length(y)
+  global bin_i
+  # global bin_logloss
+
+  if ŷ[i] > bins_max[bin_i]
+    bin_i += 1
+    # bin_logloss = 0.0
+  end
+
+  bins_Σŷ[bin_i]      += ŷ[i] * weights[i]
+  bins_Σy[bin_i]      += y[i] * weights[i]
+  bins_Σweight[bin_i] += weights[i]
+
+  # bin_logloss += logloss(y[i], ŷ[i])
+
+  # if bin_logloss >= per_bin_logloss
+  #   bins_max[bin_i] = ŷ[i]
+  # end
+  if bins_Σy[bin_i] >= per_bin_pos_weight
+    bins_max[bin_i] = ŷ[i]
+  end
+end
+
+println("mean_y\tmean_ŷ\tΣweight\tbin_max")
+for bin_i in 1:length(bins_Σy)
+  Σŷ      = bins_Σŷ[bin_i]
+  Σy      = bins_Σy[bin_i]
+  Σweight = bins_Σweight[bin_i]
+
+  mean_ŷ = Σŷ / Σweight
+  mean_y = Σy / Σweight
+
+  println("$mean_y\t$mean_ŷ\t$Σweight\t$(bins_max[bin_i])")
+end
+
+# mean_y          mean_ŷ          Σweight         bin_max
+
+
+roc_auc(X[:,1], y, weights) #
+
+σ(x) = 1.0f0 / (1.0f0 + exp(-x))
+
+logit(p) = log(p / (one(p) - p))
+
+
+# Plan:
+# 0. x Use all SREF forecasts, not just 0Z
+# 1. blur SREF to maximize AUC (hour-based)
+# 2. blur RAP to maximize AUC (hour-based)
+# 3. bin RAP predictions into 10 bins of equal weight of positive labels
+# 4. combine bin-pairs (overlapping, 9 bins total)
+# 5. train a logistic regression for each bin, σ(a1*logit(RAP) + a2*logit(SREF) + a3*logit(RAP)*logit(SREF) + a4*max(logit(RAP),logit(SREF)) + a5*min(logit(RAP),logit(SREF)) + b)
+# (5.5. add a4*hour/36*logit(RAP) + a5*hour/36*logit(SREF) + a6*hour/36*logit(RAP)*logit(SREF) + a7*hour/36 terms + a8*logit(hour in day tor prob) + a9*logit(hour in day tor prob given severe) + a10*logit(geomean previous two)? check via cross-validation)
+# 6. prediction is weighted mean of the two overlapping logistic models
+# 7. predictions should thereby be calibrated (check)
+
+
+# For days:
+# 1. Try both independent events total prob and max hourly prob as the main descriminator
+# 2. bin predictions into 10 bins of equal weight of positive labels
+# 3. combine bin-pairs (overlapping, 9 bins total)
+# 4. train a logistic regression for each bin,
+#   σ(a1*logit(independent events total prob) +
+#     a2*logit(max hourly prob) +
+#     a3*logit(2nd highest hourly prob) +
+#     a4*logit(3rd highest hourly prob) +
+#     a5*logit(4th highest hourly prob) +
+#     a6*logit(5th highest hourly prob) +
+#     a7*logit(6th highest hourly prob) +
+#     a8*logit(tornado day climatological prob) +
+#     a9*logit(tornado day given severe day climatological prob) +
+#     a10*logit(geomean(above two)) +
+#     a11*logit(tornado prob for given month) +
+#     a12*logit(tornado prob given severe day for given month) +
+#     a13*logit(geomean(above two)) +
+#     b)
+#   Check & eliminate terms via 3-fold cross-validation.
+# 5. prediction is weighted mean of the two overlapping logistic models
+# 6. should thereby be absolutely calibrated (check)
+# 7. calibrate to SPC thresholds (linear interpolation)
+
+# Don't worry about tiny regions (yet)
+#
+# Compare to SPC over validation set
+
+println("Determining best blur radii")
+
+push!(LOAD_PATH, (@__DIR__) * "/../../lib")
+import Forecasts
+import Inventories
+inventory = Forecasts.inventory(validation_forecasts[1])
+
+# for j in 1:(length(inventory) - 5)
+#   println("$j $(Inventories.inventory_line_description(inventory[j]))")
+#   println("AUC: $(roc_auc((@view X[:,j]), y, weights))")
+# end
+
+blur_radii = [0; RAPPrediction.blur_radii]
+forecast_hour_j = length(inventory)
+
+println("blur_radius_f2\tblur_radius_f17\tAUC")
+
+total_weight    = sum(Float64.(weights))
+positive_weight = sum(y .* Float64.(weights))
+
+X_blurred = Array{Float32}(undef, length(y))
+
+# Mutates X_blurred
+function does_this_need_to_be_a_function_to_be_fast(X_blurred, X, blur_i_lo, blur_i_hi, forecast_hour_j)
+  Threads.@threads for i in 1:length(y)
+    forecast_ratio = (X[i,forecast_hour_j] - 2f0) * (1f0/(17f0-2f0))
+    X_blurred[i] = X[i,blur_i_lo] * (1f0 - forecast_ratio) + X[i,blur_i_hi] * forecast_ratio
+  end
+end
+
+for blur_i_lo in 1:length(blur_radii)
+  for blur_i_hi in 1:length(blur_radii)
+
+    does_this_need_to_be_a_function_to_be_fast(X_blurred, X, blur_i_lo, blur_i_hi, forecast_hour_j)
+
+    auc = Metrics.roc_auc(X_blurred, y, weights; total_weight = total_weight, positive_weight = positive_weight)
+
+    println("$(blur_radii[blur_i_lo])\t$(blur_radii[blur_i_hi])\t$(Float32(auc))")
+  end
+end
+
+X_blurred = nothing
+
+# blur_radius_f2  blur_radius_f17 AUC
+
+
+
+# Sanity check. Should be
+
+import Dates
+
+push!(LOAD_PATH, (@__DIR__) * "/../shared")
+# import TrainGBDTShared
+import TrainingShared
+import Metrics
+
+push!(LOAD_PATH, @__DIR__)
+import RAPPrediction
+
+push!(LOAD_PATH, (@__DIR__) * "/../../lib")
+import Forecasts
+
+(_, validation_forecasts_blurred, _) = TrainingShared.forecasts_train_validation_test(RAPPrediction.forecasts_blurred(); just_hours_near_storm_events = false);
+
+# We don't have storm events past this time.
+cutoff = Dates.DateTime(2020, 11, 1, 0)
+validation_forecasts_blurred = filter(forecast -> Forecasts.valid_utc_datetime(forecast) < cutoff, validation_forecasts_blurred);
+validation_forecasts_blurred = filter(forecast -> forecast.forecast_hour in 2:17, validation_forecasts_blurred);
+
+# Make sure a forecast loads
+Forecasts.data(validation_forecasts_blurred[100])
+
+X2, y2, weights2 = TrainingShared.get_data_labels_weights(validation_forecasts_blurred; save_dir = "validation_forecasts_blurred");
+
+Float32(Metrics.roc_auc((@view X2[:,1]), y2, weights2)) # Expected: ...
+# ...
