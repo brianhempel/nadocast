a715f4a7f8392c38d3b740e3542a28bf93c6aa08
Brian Hempel <bnhempel@mtu.edu>
2021-08-10 01:49:38 -0500

    caching so three hour windows compute faster

--- lib/ForecastCombinators.jl
+++ lib/ForecastCombinators.jl
@@ -131,7 +131,7 @@ _cached_data        = Tuple{Forecasts.Forecast,Array{Float32,2}}[]
 
 function turn_forecast_caching_on()
   global _caching_on
-  # _caching_on = true
+  _caching_on = false
 end
 
 function turn_forecast_caching_off()
