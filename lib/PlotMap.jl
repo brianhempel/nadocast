7a3744638c841d0bde29b3e97a8c39ea0a6a2398
Brian Hempel <bnhempel@mtu.edu>
2021-08-14 00:26:42 -0500

    colorbar…

--- lib/PlotMap.jl
+++ lib/PlotMap.jl
@@ -194,7 +194,7 @@ function plot_map(base_path, grid, vals; run_time_utc=nothing, forecast_hour_ran
       println(f, "EOF")
     end
 
-    println(f, "gmt colorbar --FONT_ANNOT_PRIMARY=4p,Helvetica --MAP_FRAME_PEN=0.5p --MAP_TICK_PEN_PRIMARY=0.5p -Dn0.75/0.95i+w1.3i+h -S -L -Np -Cprob_colors.cpt")
+    println(f, "gmt colorbar --FONT_ANNOT_PRIMARY=4p,Helvetica --MAP_FRAME_PEN=0.5p --MAP_TICK_PEN_PRIMARY=0.5p -Dn0.75/0.95i+w1.3i+h -S -L -Np -C$colors_path")
     println(f, "gmt end")
 
     println(f, "pdftoppm $base_path.pdf $base_path -png -r 300 -singlefile")
