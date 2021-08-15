75aa3516101bc2ffb7399a10572d397de5d6e171
Brian Hempel <bnhempel@mtu.edu>
2021-08-15 18:57:14 -0500

    map annotations

--- lib/PlotMap.jl
+++ lib/PlotMap.jl
@@ -189,12 +189,13 @@ function plot_map(base_path, grid, vals; run_time_utc=nothing, forecast_hour_ran
         end
         println(f, "L 4pt,Helvetica C $sources_str")
       end
-      println(f, "L 5pt,Helvetica C 2020 Models")
+      println(f, "L 4pt,Helvetica C 2020 Models")
       println(f, "L 4pt,Helvetica,gray C @_nadocast.com@_")
       println(f, "EOF")
     end
 
-    println(f, "gmt colorbar --FONT_ANNOT_PRIMARY=4p,Helvetica --MAP_FRAME_PEN=0 --MAP_TICK_LENGTH_PRIMARY=0 --MAP_TICK_PEN_PRIMARY=0 -Dn0.55/0.1+w1.4i/0.1i+h -S -L -Np -C$colors_path")
+    println(f, "gmt colorbar --FONT_ANNOT_PRIMARY=4p,Helvetica --MAP_FRAME_PEN=0i --MAP_TICK_LENGTH_PRIMARY=0i --MAP_TICK_PEN_PRIMARY=0 -Dn0.54/0.04+w1.4i/0.1i+h -S -L0i -Np -C$colors_path")
+    println(f, "echo '0.54 0.1 Chance of a tornado within 25 miles of a point.' | gmt text -JX1/1 -R0/1/0/1 -F+f4p,Helvetica+jLB")
     println(f, "gmt end")
 
     println(f, "pdftoppm $base_path.pdf $base_path -png -r 300 -singlefile")
