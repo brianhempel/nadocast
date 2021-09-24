7466fcb6d0c527518b95b226f1e6242613397551
Brian Hempel <bnhempel@mtu.edu>
2021-09-23 23:19:53 -0500

    SPC outlooks through June 2021

--- spc/get_outlooks.rb
+++ spc/get_outlooks.rb
@@ -3,10 +3,10 @@ require 'date'
 
 # https://www.spc.noaa.gov/products/outlook/archive/2019/day1otlk_20190812_1300-shp.zip
 
-# 2019-01-7 through 2021-5-31
+# 2019-01-7 through 2021-6-30
 
-DATES = (Date.new(2019,1,7)..Date.new(2021,5,31)).to_a
-HOURS_TO_GET = %w[1200 1300 1630] # 0600Z 1300Z and 1630Z SPC outlooks, corresponding to 0Z 10Z and 14Z Nadocast forecasts. I'm sure there's some weird legacy reason why the 0600 is call 1200
+DATES = (Date.new(2019,1,7)..Date.new(2021,6,30)).to_a
+HOURS_TO_GET = %w[1200 1300 1630] # 0600Z 1300Z and 1630Z SPC outlooks, corresponding to 0Z 10Z and 14Z Nadocast forecasts.
 
 class Date
   def year_month_day
