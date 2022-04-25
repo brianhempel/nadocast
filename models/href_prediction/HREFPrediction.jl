61d1ee9baafa926a5f3450c718e8a724e53acd1f
Brian Hempel <bnhempel@mtu.edu>
2022-04-25 10:30:16 -0500

    trying some HREF models with same hyperparameter space as SREF

--- models/href_prediction/HREFPrediction.jl
+++ models/href_prediction/HREFPrediction.jl
@@ -68,14 +68,14 @@ end
 
 # (event_name, grib2_var_name, gbdt_f2_to_f13, gbdt_f13_to_f24, gbdt_f24_to_f35)
 models = [
-  # ("tornado", "TORPROB", "gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-04-16T10.56.27.856_tornado/391_trees_loss_0.0010360148.model",
-  #                        "gbdt_3hr_window_3hr_min_mean_max_delta_f13-24_2022-04-19T11.41.57.211_tornado/317_trees_loss_0.001094988.model",
-  #                        "gbdt_3hr_window_3hr_min_mean_max_delta_f24-35_2022-04-16T14.36.46.241_tornado/308_trees_loss_0.0011393429.model"
-  # ),
+  ("tornado", "TORPROB", "gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-04-16T10.56.27.856_tornado/391_trees_loss_0.0010360148.model",
+                         "gbdt_3hr_window_3hr_min_mean_max_delta_f13-24_2022-04-19T11.41.57.211_tornado/317_trees_loss_0.001094988.model",
+                         "gbdt_3hr_window_3hr_min_mean_max_delta_f24-35_2022-04-16T14.36.46.241_tornado/308_trees_loss_0.0011393429.model"
+  ),
   # The below tor model was trained with same hyperparameter space as the SREF
-  ("tornado", "TORPROB", "gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-04-23T20.18.52.994_tornado/342_trees_loss_0.001037691.model",
-                         "gbdt_3hr_window_3hr_min_mean_max_delta_f13-24_2022-04-24T08.58.17.687_tornado/285_trees_loss_0.0010958544.model",
-                         "gbdt_3hr_window_3hr_min_mean_max_delta_f24-35_2022-04-24T20.47.08.891_tornado/326_trees_loss_0.0011457962.model"
+  ("tornado_higher_feat_fract", "TORPROB", "gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-04-23T20.18.52.994_tornado/342_trees_loss_0.001037691.model",
+                                           "gbdt_3hr_window_3hr_min_mean_max_delta_f13-24_2022-04-24T08.58.17.687_tornado/285_trees_loss_0.0010958544.model",
+                                           "gbdt_3hr_window_3hr_min_mean_max_delta_f24-35_2022-04-24T20.47.08.891_tornado/326_trees_loss_0.0011457962.model"
   ),
   ("wind", "WINDPROB", "gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-04-16T10.56.27.856_wind/754_trees_loss_0.0062351814.model",
                        "gbdt_3hr_window_3hr_min_mean_max_delta_f13-24_2022-04-19T11.41.57.211_wind/581_trees_loss_0.00660574.model",
