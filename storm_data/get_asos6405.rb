433ecf2214abcb6ddd81aefeb4db139147d16d3c
Brian Hempel <bnhempel@mtu.edu>
2022-06-22 15:47:49 -0500

    get asos again but ensure we actually download the files

--- storm_data/get_asos6405.rb
+++ storm_data/get_asos6405.rb
@@ -13,6 +13,7 @@ start_year, start_month = (ENV["START_YEAR_MONTH"] || "2000-1").split("-").map(&
 
 HERE           = File.expand_path("..", __FILE__)
 PROCESS_SCRIPT = File.expand_path("../process_asos6405.rb", __FILE__)
+UNDOWNLOADABLE = File.expand_path("../undownloadable_asos.txt", __FILE__)
 
 def out_path(year, month)
   File.join(HERE, "gusts-%04d-%02d.csv" % [year, month])
@@ -35,8 +36,9 @@ system("ruby mshr_enhanced_to_csv.rb > mshr_enhanced_wban_only.csv")
 def wait_for_prompt(ftp)
   buf = ""
   loop do
-    buf = ftp.readpartial(1_000_000)
-    print buf
+    more_buf = ftp.readpartial(1_000_000)
+    buf << more_buf
+    print more_buf
     loop do
       sleep(0.5) # Data might still be coming
       break unless more_buf = (fswatch.read_nonblock(1_000_000) rescue nil)
@@ -46,6 +48,7 @@ def wait_for_prompt(ftp)
     end
     break if buf.end_with?("ftp> ")
   end
+  buf
 end
 
 def ftp_do(ftp, command)
@@ -55,16 +58,56 @@ def ftp_do(ftp, command)
 end
 
 puts "What's your email address?"
-email = gets.strip
+EMAIL = gets.strip
 
 exit(1) unless email =~ /\S+@\S+\.\S+/
 
+def connect_and_do(year, cmd)
+  out = nil
+  IO.popen("ftp --no-prompt --prompt -n ftp.ncdc.noaa.gov", "r+") do |ftp|
+    wait_for_prompt(ftp)
+    ftp_do(ftp, "user ftp #{EMAIL}") # password is email address
+    ftp_do(ftp, "cd /pub/data/asos-onemin/6405-#{year}")
+    out = ftp_do(ftp, cmd).sub("#{cmd}\n", "").sub("ftp> ", "")
+    ftp.puts("quit")
+    puts "quit"
+  end
+  out
+end
+
+File.write(UNDOWNLOADABLE, "") unless ENV["START_YEAR_MONTH"] # clear the file
+
 process_thread = nil
 (2000..2022).each do |year|
+  next if year < start_year
+
+  year_listing = connect_and_do(year, "ls")
+
+  year_files_to_get_and_size =
+    year_listing.lines.map do |l|
+      [l.split.last, l[32..41].to_i]
+    end
+
+  while year_files_to_get_and_size.size < 3015
+    sleep 60
+
+    year_listing = connect_and_do(year, "ls")
+
+    year_files_to_get_and_size =
+      year_listing.lines.map do |l|
+        [l.split.last, l[32..41].to_i]
+      end
+  end
+
   (1..12).each do |month|
     next if ([year, month] <=> [start_year, start_month]) < 0
 
     puts "#{year}-#{month}"
+
+    month_files_to_get_and_size =
+      year_files_to_get_and_size.select do |name, size|
+        name.end_with?("%02d.dat" % month)
+      end
 
     FileUtils.cd HERE
 
@@ -96,13 +139,22 @@ process_thread = nil
     FileUtils.mkdir_p month_dir
     FileUtils.cd month_dir
 
-    IO.popen("ftp --no-prompt --prompt -n ftp.ncdc.noaa.gov", "r+") do |ftp|
-      wait_for_prompt(ftp)
-      ftp_do(ftp, "user ftp #{email}") # password is email address
-      ftp_do(ftp, "cd /pub/data/asos-onemin/6405-#{year}")
-      ftp_do(ftp, "mget *%02d.dat" % month)
-      ftp.puts "quit"
-      puts "quit"
+    connect_and_do(year, "mget *%02d.dat" % month)
+
+    # max 2 retries
+    2.times do
+      month_files_to_get_and_size.select! do |filename, size|
+        (File.size(filename) rescue 0) != size
+      end
+      break if month_files_to_get_and_size.size == 0
+      month_files_to_get_and_size.each do |filename, _|
+        connect_and_do(year, "get #{filename}")
+      end
+    end
+    File.open(UNDOWNLOADABLE, "a") do |undownloadable|
+      month_files_to_get_and_size.each do |filename, _|
+        undownloadable.puts(filename)
+      end
     end
     process_thread.join if process_thread
 
