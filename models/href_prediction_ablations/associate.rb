89483608c7073c91ca959153cb06fdf23692459a
Brian Hempel <bnhempel@mtu.edu>
2022-09-30 12:02:32 -0500

    f2-13 tornado ablations

--- models/href_prediction_ablations/associate.rb
+++ models/href_prediction_ablations/associate.rb
@@ -167,6 +167,90 @@ scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f13-24_2022-
 707_trees_loss_0.0012815231.model                                                                                                                                                                                                        100%   21MB   8.5MB/s   00:02
 git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f13-24_2022-09-28T23.26.38.345_tornado_excluding_13773_features_4410986610349231978/707_trees_loss_0.0012815231.model
 gbdt_3hr_window_3hr_min_mean_max_delta_f13-24_2022-09-28T23.26.38.345_tornado_excluding_13773_features_4410986610349231978/707_trees_loss_0.0012815231.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_blurs_n=910.txt_tornado_63_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_blurs_n=910.txt_tornado_63_stdout.txt                                                                                             100% 3323KB   4.6MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.20.32.745_tornado_excluding_12921_features_7409514120157424229
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.20.32.745_tornado_excluding_12921_features_7409514120157424229/868_trees_loss_0.0011486912.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.20.32.745_tornado_excluding_12921_features_7409514120157424229/868_trees_loss_0.0011486912.model
+868_trees_loss_0.0011486912.model                                                                                                                                                                          100%   23MB   8.1MB/s   00:02
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.20.32.745_tornado_excluding_12921_features_7409514120157424229/868_trees_loss_0.0011486912.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.20.32.745_tornado_excluding_12921_features_7409514120157424229/868_trees_loss_0.0011486912.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_prior_next_hrs_n=691.txt_tornado_68_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_prior_next_hrs_n=691.txt_tornado_68_stdout.txt                                                                                    100% 4068KB   4.3MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.58.07.439_tornado_excluding_13140_features_3720177259145965027
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.58.07.439_tornado_excluding_13140_features_3720177259145965027/836_trees_loss_0.0011478734.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.58.07.439_tornado_excluding_13140_features_3720177259145965027/836_trees_loss_0.0011478734.model
+836_trees_loss_0.0011478734.model                                                                                                                                                                          100%   24MB   7.7MB/s   00:03
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.58.07.439_tornado_excluding_13140_features_3720177259145965027/836_trees_loss_0.0011478734.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.58.07.439_tornado_excluding_13140_features_3720177259145965027/836_trees_loss_0.0011478734.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_3hr_n=1567.txt_tornado_67_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_3hr_n=1567.txt_tornado_67_stdout.txt                                                                                              100% 5192KB   5.5MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T09.10.44.152_tornado_excluding_12264_features_11093869335794226923
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T09.10.44.152_tornado_excluding_12264_features_11093869335794226923/1246_trees_loss_0.0011406931.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T09.10.44.152_tornado_excluding_12264_features_11093869335794226923/1246_trees_loss_0.0011406931.model
+1246_trees_loss_0.0011406931.model                                                                                                                                                                         100%   25MB   7.1MB/s   00:03
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T09.10.44.152_tornado_excluding_12264_features_11093869335794226923/1246_trees_loss_0.0011406931.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T09.10.44.152_tornado_excluding_12264_features_11093869335794226923/1246_trees_loss_0.0011406931.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_n=253.txt_tornado_62_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_n=253.txt_tornado_62_stdout.txt                                                                                                   100% 4014KB   5.4MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.03.56.368_tornado_excluding_13578_features_1255749354977721151
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.03.56.368_tornado_excluding_13578_features_1255749354977721151/1135_trees_loss_0.0011634461.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.03.56.368_tornado_excluding_13578_features_1255749354977721151/1135_trees_loss_0.0011634461.model
+1135_trees_loss_0.0011634461.model                                                                                                                                                                         100%   25MB   7.7MB/s   00:03
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.03.56.368_tornado_excluding_13578_features_1255749354977721151/1135_trees_loss_0.0011634461.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T12.03.56.368_tornado_excluding_13578_features_1255749354977721151/1135_trees_loss_0.0011634461.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_partial_climatology_n=227.txt_tornado_61_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_partial_climatology_n=227.txt_tornado_61_stdout.txt                                                                                           100% 4080KB   5.8MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.53.56.474_tornado_excluding_13604_features_16220645629827272623
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.53.56.474_tornado_excluding_13604_features_16220645629827272623/1158_trees_loss_0.0011694039.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.53.56.474_tornado_excluding_13604_features_16220645629827272623/1158_trees_loss_0.0011694039.model
+1158_trees_loss_0.0011694039.model                                                                                                                                                                         100%   27MB   8.9MB/s   00:03
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.53.56.474_tornado_excluding_13604_features_16220645629827272623/1158_trees_loss_0.0011694039.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.53.56.474_tornado_excluding_13604_features_16220645629827272623/1158_trees_loss_0.0011694039.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_n=220.txt_tornado_60_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_n=220.txt_tornado_60_stdout.txt                                                                                                               100% 3693KB   4.9MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.29.03.470_tornado_excluding_13611_features_10698383838099131809
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.29.03.470_tornado_excluding_13611_features_10698383838099131809/1041_trees_loss_0.0011743465.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.29.03.470_tornado_excluding_13611_features_10698383838099131809/1041_trees_loss_0.0011743465.model
+1041_trees_loss_0.0011743465.model                                                                                                                                                                         100%   25MB   8.5MB/s   00:02
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.29.03.470_tornado_excluding_13611_features_10698383838099131809/1041_trees_loss_0.0011743465.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T11.29.03.470_tornado_excluding_13611_features_10698383838099131809/1041_trees_loss_0.0011743465.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_grads_n=1348.txt_tornado_64_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_grads_n=1348.txt_tornado_64_stdout.txt                                                                                            100% 3635KB   6.1MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.51.679_tornado_excluding_12483_features_2140312678839727709
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.51.679_tornado_excluding_12483_features_2140312678839727709/793_trees_loss_0.0011474881.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.51.679_tornado_excluding_12483_features_2140312678839727709/793_trees_loss_0.0011474881.model
+793_trees_loss_0.0011474881.model                                                                                                                                                                          100%   22MB   8.8MB/s   00:02
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.51.679_tornado_excluding_12483_features_2140312678839727709/793_trees_loss_0.0011474881.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.51.679_tornado_excluding_12483_features_2140312678839727709/793_trees_loss_0.0011474881.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_blurs_grads_n=2005.txt_tornado_65_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_climatology_blurs_grads_n=2005.txt_tornado_65_stdout.txt                                                                                      100% 3304KB   5.7MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.46.15.004_tornado_excluding_11826_features_14576562183048218741
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.46.15.004_tornado_excluding_11826_features_14576562183048218741/523_trees_loss_0.0011400548.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.46.15.004_tornado_excluding_11826_features_14576562183048218741/523_trees_loss_0.0011400548.model
+523_trees_loss_0.0011400548.model                                                                                                                                                                          100%   21MB   8.6MB/s   00:02
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.46.15.004_tornado_excluding_11826_features_14576562183048218741/523_trees_loss_0.0011400548.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.46.15.004_tornado_excluding_11826_features_14576562183048218741/523_trees_loss_0.0011400548.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_prob_n=80.txt_tornado_57_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_prob_n=80.txt_tornado_57_stdout.txt                                                                                                                              100% 4614KB   4.4MB/s   00:01
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.550_tornado_excluding_13751_features_16850198170780752800
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.550_tornado_excluding_13751_features_16850198170780752800/812_trees_loss_0.0012124014.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.550_tornado_excluding_13751_features_16850198170780752800/812_trees_loss_0.0012124014.model
+812_trees_loss_0.0012124014.model                                                                                                                                                                          100%   19MB   7.1MB/s   00:02
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.550_tornado_excluding_13751_features_16850198170780752800/812_trees_loss_0.0012124014.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.550_tornado_excluding_13751_features_16850198170780752800/812_trees_loss_0.0012124014.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_n=138.txt_tornado_58_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_n=138.txt_tornado_58_stdout.txt                                                                                                                        100% 4156KB   5.1MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.590_tornado_excluding_13693_features_7893827997600368035
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.590_tornado_excluding_13693_features_7893827997600368035/1207_trees_loss_0.0011827772.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.590_tornado_excluding_13693_features_7893827997600368035/1207_trees_loss_0.0011827772.model
+1207_trees_loss_0.0011827772.model                                                                                                                                                                         100%   18MB   6.8MB/s   00:02
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.590_tornado_excluding_13693_features_7893827997600368035/1207_trees_loss_0.0011827772.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.590_tornado_excluding_13693_features_7893827997600368035/1207_trees_loss_0.0011827772.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_no_sv_n=219.txt_tornado_59_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_prob_computed_no_sv_n=219.txt_tornado_59_stdout.txt                                                                                                         100% 3730KB   5.9MB/s   00:00
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.728_tornado_excluding_13612_features_12247408129229835884
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.728_tornado_excluding_13612_features_12247408129229835884/779_trees_loss_0.0011766684.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.728_tornado_excluding_13612_features_12247408129229835884/779_trees_loss_0.0011766684.model
+779_trees_loss_0.0011766684.model                                                                                                                                                                          100%   22MB   6.9MB/s   00:03
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.728_tornado_excluding_13612_features_12247408129229835884/779_trees_loss_0.0011766684.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.728_tornado_excluding_13612_features_12247408129229835884/779_trees_loss_0.0011766684.model
+scp nadocaster2-remote-raw:~/train_href_f2-13_0.03_0.4_features_2021v2_mean_n=58.txt_tornado_56_stdout.txt ./
+train_href_f2-13_0.03_0.4_features_2021v2_mean_n=58.txt_tornado_56_stdout.txt                                                                                                                              100% 4644KB   4.5MB/s   00:01
+mkdir gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.584_tornado_excluding_13773_features_4410986610349231978
+scp nadocaster2-remote-raw:~/gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.584_tornado_excluding_13773_features_4410986610349231978/700_trees_loss_0.0012174041.model ./gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.584_tornado_excluding_13773_features_4410986610349231978/700_trees_loss_0.0012174041.model
+700_trees_loss_0.0012174041.model                                                                                                                                                                          100%   19MB   6.8MB/s   00:02
+git add -f gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.584_tornado_excluding_13773_features_4410986610349231978/700_trees_loss_0.0012174041.model
+gbdt_3hr_window_3hr_min_mean_max_delta_f2-13_2022-09-30T08.43.27.584_tornado_excluding_13773_features_4410986610349231978/700_trees_loss_0.0012174041.model
 STUFF
 
 
@@ -182,7 +266,9 @@ mean_prob_computed_climatology_grads_n=1348
 mean_prob_computed_climatology_blurs_grads_n=2005
 mean_prob_computed_climatology_prior_next_hrs_n=691
 mean_prob_computed_climatology_3hr_n=1567".split.each do |experiment|
-  blob_f13_24 = stuff.split("scp ").find { |blob| blob.include?(experiment) && blob.include?("_f13-24_") }
-  blob_f24_35 = stuff.split("scp ").find { |blob| blob.include?(experiment) && blob.include?("_f24-35_") }
-  puts "\"#{blob_f13_24[/gbdt_.*/]}\", \"#{blob_f24_35[/gbdt_.*/]}\","
+  blob_f2_13  = stuff.split(/scp.*stdout/).find { |blob| blob.include?(experiment) && blob.include?("_f2-13_") }
+  blob_f13_24 = stuff.split(/scp.*stdout/).find { |blob| blob.include?(experiment) && blob.include?("_f13-24_") }
+  blob_f24_35 = stuff.split(/scp.*stdout/).find { |blob| blob.include?(experiment) && blob.include?("_f24-35_") }
+  # puts blob_f2_13[/gbdt_\S+model/]
+  puts "\"#{blob_f2_13[/gbdt_\S+model/]}\", \"#{blob_f13_24[/gbdt_\S+model/]}\", \"#{blob_f24_35[/gbdt_\S+model/]}\","
 end
