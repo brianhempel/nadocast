62798cd64e260aa924f72d1cee04140da5b34370
Brian Hempel <bnhempel@mtu.edu>
2022-10-06 12:00:09 -0500

    More SPC outlooks, to match the date range of our HREF archive (≥2018-6-29) and the storm events through May 2022

--- spc/get_outlooks.rb
+++ spc/get_outlooks.rb
@@ -3,31 +3,33 @@ require 'date'
 
 # https://www.spc.noaa.gov/products/outlook/archive/2019/day1otlk_20190812_1300-shp.zip
 
-# 2019-01-7 through 2021-12-31
+# 2018-6-29 through 2022-5-31
 
 # NOTE: I had to manually fix day1otlk_20200409_1300_wind.shp and day1otlk_20200407_1630_wind.shp and day1otlk_20200409_1630_wind.shp with the "Fix Geometries" tool in the "Processing Toolbox" pane in QGIS.
 
-DATES = (Date.new(2019,1,7)..Date.new(2022,2,22)).to_a
-DATES = (Date.new(2021,12,31)..Date.new(2022,2,22)).to_a
+# DATES = (Date.new(2018,6,29)..Date.new(2019,1,7)).to_a
+# DATES = (Date.new(2019,1,7)..Date.new(2022,2,22)).to_a
+# DATES = (Date.new(2021,12,31)..Date.new(2022,2,22)).to_a
+DATES = (Date.new(2022,2,22)..Date.new(2022,5,31)).to_a
 HOURS_TO_GET = %w[1200 1300 1630] # 0600Z 1300Z and 1630Z SPC outlooks, corresponding to 0Z 10Z and 14Z Nadocast forecasts.
 
 class Date
-  def year_month_day
+  def yyyymmdd
     "%04d%02d%02d" % [year, month, day]
   end
 
-  def year_month
+  def yyyymm
     "%04d%02d" % [year, month]
   end
 end
 
 DATES.each do |date|
   HOURS_TO_GET.each do |hour|
-    file_name  = "day1otlk_#{date.year_month_day}_#{hour}-shp.zip"
+    file_name  = "day1otlk_#{date.yyyymmdd}_#{hour}-shp.zip"
     url_to_get = "https://www.spc.noaa.gov/products/outlook/archive/#{date.year}/#{file_name}"
-    dir = date.year_month
+    dir = date.yyyymm
     path       = "#{dir}/#{file_name}"
-    if (File.size("#{dir}/day1otlk_#{date.year_month_day}_#{hour}_torn.shp") rescue 0) == 0
+    if (File.size("#{dir}/day1otlk_#{date.yyyymmdd}_#{hour}_torn.shp") rescue 0) == 0
       system("mkdir -p #{dir} 2> /dev/null")
       puts "#{url_to_get} -> #{path}"
       data = `curl -f -s --show-error #{url_to_get}`
