5a4875e350b30e9ebd9e7d4bb7bf0c76bb822078
Brian Hempel <bnhempel@mtu.edu>
2022-12-01 13:48:27 -0600

    SPC outlooks back to 2011-1-1 for making a climatology

--- spc/get_outlooks.rb
+++ spc/get_outlooks.rb
@@ -10,7 +10,8 @@ require 'date'
 # DATES = (Date.new(2018,6,29)..Date.new(2019,1,7)).to_a
 # DATES = (Date.new(2019,1,7)..Date.new(2022,2,22)).to_a
 # DATES = (Date.new(2021,12,31)..Date.new(2022,2,22)).to_a
-DATES = (Date.new(2022,6,1)..Date.new(2022,7,31)).to_a
+# DATES = (Date.new(2022,6,1)..Date.new(2022,7,31)).to_a
+DATES = (Date.new(2011,1,1)..Date.new(2018,6,29)).to_a
 HOURS_TO_GET = %w[1200 1300 1630] # 0600Z 1300Z and 1630Z SPC outlooks, corresponding to 0Z 10Z and 14Z Nadocast forecasts.
 
 class Date
@@ -24,17 +25,19 @@ class Date
 end
 
 DATES.each do |date|
-  HOURS_TO_GET.each do |hour|
-    file_name  = "day1otlk_#{date.yyyymmdd}_#{hour}-shp.zip"
-    url_to_get = "https://www.spc.noaa.gov/products/outlook/archive/#{date.year}/#{file_name}"
-    dir = date.yyyymm
-    path       = "#{dir}/#{file_name}"
-    if (File.size("#{dir}/day1otlk_#{date.yyyymmdd}_#{hour}_torn.shp") rescue 0) == 0
-      system("mkdir -p #{dir} 2> /dev/null")
-      puts "#{url_to_get} -> #{path}"
-      data = `curl -f -s --show-error #{url_to_get}`
-      File.write(path, data) if $?.success?
-      system("unzip -d #{dir} #{path} && rm #{path}")
+  HOURS_TO_GET.map do |hour|
+    Thread.new do
+      file_name  = "day1otlk_#{date.yyyymmdd}_#{hour}-shp.zip"
+      url_to_get = "https://www.spc.noaa.gov/products/outlook/archive/#{date.year}/#{file_name}"
+      dir = date.yyyymm
+      path       = "#{dir}/#{file_name}"
+      if (File.size("#{dir}/day1otlk_#{date.yyyymmdd}_#{hour}_torn.shp") rescue 0) == 0
+        system("mkdir -p #{dir} 2> /dev/null")
+        puts "#{url_to_get} -> #{path}"
+        data = `curl -f -s --show-error #{url_to_get}`
+        File.write(path, data) if $?.success?
+        system("unzip -d #{dir} #{path} && rm #{path}")
+      end
     end
-  end
+  end.map(&:join)
 end
