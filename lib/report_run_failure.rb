b619167116b9690040cf403bac18b66779716242
Brian Hempel <git@brianhempel.com>
2026-02-28 12:08:34 -0800

    asdf

--- lib/report_run_failure.rb
+++ lib/report_run_failure.rb
@@ -44,20 +44,20 @@ report_cmd_result(body, "df -h")
 report_cmd_result(body, "top -c -b -n 1 -o %CPU | head -30")
 body << "\n"
 body << "\n"
-report_mtime(body, "/home/brian/nadocast_dev/forecaster.log")
-report_cmd_result(body, "tail -100 /home/brian/nadocast_dev/forecaster.log")
-report_mtime(body, "/home/brian/nadocast_dev/get_data/get_href.log")
-report_cmd_result(body, "tail -20 /home/brian/nadocast_dev/get_data/get_href.log")
-report_mtime(body, "/home/brian/nadocast_dev/get_data/get_sref.log")
-report_cmd_result(body, "tail -20 /home/brian/nadocast_dev/get_data/get_sref.log")
-body << "\n"
-body << "\n"
-report_mtime(body, "/home/brian/nadocast_operational_2020/forecaster.log")
-report_cmd_result(body, "tail -100 /home/brian/nadocast_operational_2020/forecaster.log")
-report_mtime(body, "/home/brian/nadocast_operational_2020/get_data/get_href.log")
-report_cmd_result(body, "tail -20 /home/brian/nadocast_operational_2020/get_data/get_href.log")
-report_mtime(body, "/home/brian/nadocast_operational_2020/get_data/get_sref.log")
-report_cmd_result(body, "tail -20 /home/brian/nadocast_operational_2020/get_data/get_sref.log")
+# report_mtime(body, "/home/brian/nadocast_dev/forecaster.log")
+# report_cmd_result(body, "tail -100 /home/brian/nadocast_dev/forecaster.log")
+# report_mtime(body, "/home/brian/nadocast_dev/get_data/get_href.log")
+# report_cmd_result(body, "tail -20 /home/brian/nadocast_dev/get_data/get_href.log")
+# report_mtime(body, "/home/brian/nadocast_dev/get_data/get_sref.log")
+# report_cmd_result(body, "tail -20 /home/brian/nadocast_dev/get_data/get_sref.log")
+# body << "\n"
+# body << "\n"
+# report_mtime(body, "/home/brian/nadocast_operational_2020/forecaster.log")
+# report_cmd_result(body, "tail -100 /home/brian/nadocast_operational_2020/forecaster.log")
+# report_mtime(body, "/home/brian/nadocast_operational_2020/get_data/get_href.log")
+# report_cmd_result(body, "tail -20 /home/brian/nadocast_operational_2020/get_data/get_href.log")
+# report_mtime(body, "/home/brian/nadocast_operational_2020/get_data/get_sref.log")
+# report_cmd_result(body, "tail -20 /home/brian/nadocast_operational_2020/get_data/get_sref.log")
 
 json_str = {
   "From" => "Nadocast Bot <#{ENV["EMAIL_ADDRESS"]}>",
@@ -67,12 +67,12 @@ json_str = {
   "MessageStream" => "outbound"
 }.to_json
 
-# puts json_str
+puts json_str
 
 cmd = ["curl", "https://api.postmarkapp.com/email", "-X", "POST", "-H", '"Accept: application/json"', '-H', '"Content-Type: application/json"', '-H', '"X-Postmark-Server-Token: ', ENV['POSTMARK_SERVER_TOKEN'], "--data-binary", "@-"]
 
 IO.popen(cmd, "r+") do |io|
-  io.write(body)
+  io.write(json_str)
   io.close_write
   puts io.read
 end
